2843753d405b1b6cb580d4cf61519a32b7d45854
mohammedmutalib <125193976+mohammedmutalib@users.noreply.github.com>
2023-08-25 23:07:49 +0530

    Add files via upload

---
+++ task16.js
@@ -0,0 +1,61 @@
+function isSafe(board, row, col, num) {
+    for (let i = 0; i < 9; i++) {
+        if (board[row][i] === num || board[i][col] === num) {
+            return false;
+        }
+    }
+    const startRow = row - (row % 3);
+    const startCol = col - (col % 3);
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[startRow + i][startCol + j] === num) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+function solveSudoku(board) {
+    const emptySpot = findEmptySpot(board);
+    if (!emptySpot) {
+        return true; 
+    }
+    const [row, col] = emptySpot;
+    for (let num = 1; num <= 9; num++) {
+        if (isSafe(board, row, col, num)) {
+            board[row][col] = num;
+            if (solveSudoku(board)) {
+                return true; 
+            }
+            board[row][col] = 0;
+        }
+    }
+    return false; 
+}
+function findEmptySpot(board) {
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            if (board[row][col] === 0) {
+                return [row, col];
+            }
+        }
+    }
+    return null; 
+}
+const sudokuBoard = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+];
+if (solveSudoku(sudokuBoard)) {
+    console.log("Sudoku solved:");
+    console.log(sudokuBoard);
+} else {
+    console.log("No solution exists.");
+}
